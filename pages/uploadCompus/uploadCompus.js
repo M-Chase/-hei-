09e1a481bef8e7864953b550e5ecb990a8fe1824
M-Chase <60459217+M-Chase@users.noreply.github.com>
2021-03-12 14:52:00 +0800

    发布消息页面
    前端基本部署完成

--- pages/uploadCompus/uploadCompus.js
+++ pages/uploadCompus/uploadCompus.js
@@ -6,6 +6,20 @@ Page({
    * 页面的初始数据
    */
   data: {
+    publish_info: '',//发布的信息
+    text_info: '',//文本
+    value: '',
+    tempFilePaths: [],
+    img_arr: [],
+    openid: '123',
+    timestamp: '',
+    changeClass: '闲聊',
+    comment2: '',
+    color: '#DCDCDC',
+    disabled: true,
+    title: '',
+    pub_count: 0,
+    topic:"",
     nvabarData: {
       showCapsule: 1, //是否显示左上角图标   1表示显示    0表示不显示
       title: '发布消息', //导航栏 中间的标题
@@ -13,6 +27,304 @@ Page({
     height: app.globalData.height * 2 + 20,
   },
 
+  changeClass: function (e) {
+    console.log(e)
+    this.setData({
+      changeClass: e.detail.value,
+    })
+  },
+  disabled_pub: function () {
+    var title = this.data.title;
+    var color = '#DCDCDC';
+    var disabled = true;
+    if (title.length != 0) {
+      color = "black",
+        disabled = false
+    }
+    this.setData({
+      color: color,
+      disabled: disabled
+    })
+  },
+
+  get_describe(event) {
+    // event.detail 为当前输入的值
+    this.setData({
+      text_info: event.detail.value
+    });
+    this.data.pub_count = 0;
+  },
+
+  get_title: function (e) {
+
+
+    var title = e.detail.value;
+    title = e.detail.value.replace(/\s+/g, '');
+    this.setData({
+      title: title,
+    })
+    this.disabled_pub();
+    this.data.pub_count = 0;
+  },
+  get_topic: function (e) {
+    var topic = e.detail.value;
+    this.setData({
+      topic: e.detail.value,
+    })
+  },
+  submit: function () {
+    if (this.data.pub_count > 0) {
+      console.log("已经发布了")
+      return ''
+    }
+    this.data.pub_count += 1
+
+    if (!this.data.title) {
+      util.is_valid(this.data.title, '标题不能为空！')
+    }
+    else {
+      var that = this
+      var img_arr = that.data.img_arr;
+      var text_info = that.data.text_info;
+      var openid = app.globalData.openid;
+      var timestamp = Date.parse(new Date());
+      timestamp = parseInt(timestamp / 1000);
+      var buy_sell = that.data.buy_sell;
+      var price = that.data.price;
+      if (price[price.legnth - 1] == '.') {
+        price += '00'
+      }
+      if (price == '')
+        price = '-1'
+
+      var title = that.data.title;
+      console.log('fffffffffffffffff')
+      console.log(price)
+      var formData = {
+        text_Info: text_info,
+        buy_sell: buy_sell,
+        openid: openid,
+        timestamp: timestamp,
+        price: price,
+        title: title
+      }
+      if (img_arr.length != 0)
+        that.uploadfile_img(formData, img_arr)
+      else
+        that.uploadfile(formData, '')
+
+
+    }
+
+  },
+  uploadfile_img: function (formData, filePaths) {
+    var that = this
+
+    return new Promise(function (resolve, reject) {
+      for (var i = 0; i < filePaths.length; i++) {
+        formData["pos"] = i
+        console.log(formData)
+
+        wx.uploadFile({
+          url: 'https://www.yunluheishi.cn:443/wx_Code/publish',  //填写实际接口   
+          header: {
+            "Content-Type": "application/json"
+          },
+          filePath: filePaths[i],
+          name: 'file',
+          formData: formData,
+          success: function (res) {
+            // if (res) {
+            //   wx.showToast({
+            //     title: '已提交发布！',
+            //     duration: 3000
+            //   });
+            // }
+
+            console.log('lllllllllllllllllllllllllllll')
+            console.log(res.data)
+            if (res.data == '1') {
+              wx.showModal({
+                content: "您的发布内容可能包括政治，色情，违反等有害信息,请去除后重新发布",
+                showCancel: false,
+              })
+            }
+            else {
+              console.log(i, filePaths.length)
+              if (i == filePaths.length) {
+                that.upload_data(JSON.parse(res.data))
+              }
+            }
+          },
+        })
+
+      }
+    })
+
+  },
+  uploadfile: function (formData, imgUrl) {
+    var that = this
+    formData['imgUrl'] = imgUrl
+    console.log(formData)
+    request.request('https://www.yunluheishi.cn:443/wx_Code/uploadFile', 'POST', formData).then(function (res) {
+      console.log('jjjjjjjjjjjjjjjjkkkkk')
+      console.log(res)
+      if (res['result'] == '1') {
+        wx.showModal({
+          content: "您的发布内容可能包括政治，色情，违反等有害信息,请去除后重新发布",
+          showCancel: false,
+        })
+      }
+      else {
+        that.upload_data(res)
+      }
+
+    })
+
+  },
+  to_index: function (publish_info) {
+    var pagesArr = getCurrentPages();
+
+    wx.navigateBack({
+      delta: 1,   //回退前 delta 页面
+    });
+  },
+
+
+  upload_data: function (data) {
+    var publish_idpos = data
+    console.log(data)
+    var that = this
+    // var publish_idpos = JSON.parse(data)
+    that.setData({
+      publish_info: publish_idpos['publish']
+    }),
+      app.globalData.id_pos = publish_idpos['id_pos']
+    wx.setStorage({
+      key: "publish_info",
+      data: publish_idpos['publish']
+    })
+    console.log(app.globalData.publish_info)
+    that.to_index(publish_idpos['publish'])
+
+  },
+  //从本地获取照片 
+  upimg: function () {
+    var that = this;
+    var img_arr = that.data.img_arr;
+    const imgnum = Object.keys(img_arr).length;
+    if (imgnum < 9) {
+      wx.chooseImage({
+        count: 9 - imgnum,        //一次性上传到小程序的数量     
+        sizeType: ['original', 'compressed'],  //可选择原图或压缩后的图片
+        sourceType: ['album', 'camera'], //可选择性开放访问相册、相机
+        success: res => {
+          const tempFilePaths = res.tempFilePaths
+          console.log(res.tempFilePaths)
+          var size = res.tempFiles[0].size
+          console.log("jjjjjjjjjjjjjjjj")
+          console.log(size)
+          //   that.compressImage(res.tempFilePaths,0, res.tempFiles.length)
+
+          if (size <= 3000000) {
+            that.setData({
+              //  img_arr: img_arr.concat(res.tempFilePaths), 
+              img_arr: img_arr.concat(res.tempFilePaths),
+            })
+          }
+          else {
+            wx.showModal({
+              content: "上传图片不能大于3M!",
+              showCancel: false,
+            })
+
+            wx.showModal({
+              content: "上传图片不能大于3M!",
+              showCancel: false,
+            })
+          }
+          this.disabled_pub();
+          this.data.pub_count = 0;
+
+        }
+      });
+    }
+
+  },
+  //   压缩图片
+  compressImage: function (imgUrls, nowNum, endNum) {
+    var that = this;
+    var img_arr = that.data.img_arr;
+    console.log("111")
+    if (nowNum <= endNum) {
+      if (/\.gif/gi.test(imgUrls[nowNum])) {
+        console.log("是我没错了~")
+        img_arr.push(imgUrls[nowNum])
+        nowNum += 1;
+        that.setData({
+          img_arr: img_arr,
+        })
+        that.compressImage(imgUrls, nowNum, endNum)
+      } else {
+        wx.compressImage({
+          src: imgUrls[nowNum],
+          quality: 60,
+          success: res => {
+            nowNum += 1;
+            console.log("压缩成功")
+            console.log(res)
+            img_arr.push(res.tempFilePath)
+            console.log(imgUrls)
+            that.setData({
+              img_arr: img_arr,
+            })
+            that.compressImage(imgUrls, nowNum, endNum)
+            // return res.tempFilePath
+            // console.log(that.data.img_arr)
+          }
+        })
+      }
+      //that.compressImage(imgUrls, nowNum, endNum)
+    }
+
+  },
+
+  //删除照片功能与预览照片功能 
+  deleteImg: function (e) {
+    var that = this;
+    var img_arr = that.data.img_arr;
+    var index = e.currentTarget.dataset.index;
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除此图片吗？',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('点击确定了');
+          img_arr.splice(index, 1);
+        } else if (res.cancel) {
+          console.log('点击取消了');
+          return false;
+        }
+        that.setData({
+          img_arr: img_arr
+        });
+        console.log(img_arr)
+        that.disabled_pub();
+
+      }
+    })
+
+  },
+  //预览图片
+  previewImg: function (e) {
+    console.log(e)
+    var img_arr = this.data.img_arr
+    console.log(img_arr)
+    wx.previewImage({
+      current: img_arr[e.currentTarget.dataset.imgid],
+      urls: img_arr
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
